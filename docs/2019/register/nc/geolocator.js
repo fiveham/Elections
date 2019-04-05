49d15a6991313cdf82b28ee203829bd1083ed105
fiveham <fiveham@users.noreply.github.com>
2019-04-05 14:14:36 -0400

    Create geolocator.js

---
+++ docs/2019/register/nc/geolocator.js
@@ -0,0 +1,15 @@
+function geolocate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var geolocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      var circle = new google.maps.Circle({
+        center: geolocation,
+        radius: position.coords.accuracy
+      });
+      map.fitBounds(circle.getBounds());
+    });
+  }
+}
