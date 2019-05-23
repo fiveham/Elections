7e4c104b01c434c5ecea77d21724cfb960a4832a
fiveham <fiveham@users.noreply.github.com>
2019-05-22 23:25:04 -0400

    better comments

--- docs/js/add-widgets.js
+++ docs/js/add-widgets.js
@@ -1,18 +1,26 @@
-/* Depends on: global vars 'widgets'; 'triggers'
+/* Depends on: global vars 'widgets', 'triggers', 'map' 
    widgets: a list of objects each of which has 'id' and 'where'
-      id: the value of the id attribute of the element that the widget is
-      where: a google.maps.ControlPosition
-      This list should not be populated until after the maps API is loaded
-   triggers: a list of event names */
+      id: the value of the id attribute of the element that is the widget
+      where: an integer 1 to 13 corresponding to google.maps.ControlPosition
+         Note about ControlPosition:
+         + 01    02    03 +
+         05              07
+         04      13      08
+         06              09
+         + 10    11    12 +
+   triggers: a list of event names 
+   map: the google.maps.Map map object */
 
-/* Add listed widgets to the map and make them visible (set opacity to 1) */
+/* Add listed widgets to the map and add add one-time event-listeners to the map 
+   any of which when triggered will make the widgets visible by setting each 
+   widget's opacity to 1.*/
 function add_widgets(){
   widgets.forEach(function(widget){
     var element = document.getElementById(widget.id);
     map.controls[widget.where].push(element); /* Add widget to the map */
     triggers.forEach(function(t){
       google.maps.event.addListenerOnce(map, t, function(){
-        element.style.opacity = "1"; /* Set opacity to 1 */
+        element.style.opacity = "1";
       });
     });
   });
