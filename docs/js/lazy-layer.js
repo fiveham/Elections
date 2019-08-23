50da0cde11a99076ca68df058ccd1885d948f812
fiveham <fiveham@users.noreply.github.com>
2019-08-22 22:08:50 -0400

    Create lazy-layer.js

---
+++ docs/js/lazy-layer.js
@@ -0,0 +1,32 @@
+/* Wraps multiple KmlLayers in a single object to easily 
+   turn a multi-KmlLayer layer on or off, but does not create
+   the KmlLayer objects (an action which loads the KML layer
+   data) until the first time the layer is turned on.*/
+class LazyLayer{
+  
+  constructor(urls, zIndex){
+    this.urls = urls;
+    this.zIndex = zIndex || 0
+    this.members = null;
+  }
+  
+  setMap(map){
+    if(map && !this.members){
+      for(var i=0; i<this.members.length; i++){
+        this.members.push(new google.maps.KmlLayer({
+          url: this.urls[i],
+          map: null,
+          preserveViewport: true,
+          zIndex: this.zIndex
+        }));
+      }
+    }
+    this.members.forEach(function(e){
+      e.setMap(map);
+    });
+  }
+  
+  getMap(){
+    return this.members[0].getMap();
+  }
+}
