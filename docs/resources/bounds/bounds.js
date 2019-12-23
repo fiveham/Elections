eef3b6111244c9922980ad6cfa2b95f2adc83c2f
fiveham <fiveham@users.noreply.github.com>
2019-12-23 05:20:20 -0500

    add centers

--- docs/resources/bounds/bounds.js
+++ docs/resources/bounds/bounds.js
@@ -6,10 +6,21 @@
  * a human copies a single line into a script.
  */
  
- var laBounds = {west: -94.043, east: -88.758, south: 28.855, north: 33.020};
- var ncBounds = {west: -82.745, east: -75.887, south: 34.805, north: 36.562};
- var njBounds = {west: -75.564, east: -73.885, south: 38.789, north: 41.357};
- var maBounds = {west: -73.508, east: -69.928, south: 41.239, north: 42.887};
- var msBounds = {west: -91.655, east: -88.098, south: 30.174, north: 34.996};
- var vaBounds = {west: -83.675, east: -75.166, south: 36.541, north: 39.466};
+var laBounds = {west: -94.043, east: -88.758, south: 28.855, north: 33.020};
+var laCenter = {lat: 30.9, lng: -91.4};
+
+var ncBounds = {west: -82.745, east: -75.887, south: 34.805, north: 36.562};
+var ncCenter = {lat: 35.5, lng: -79.9};
+
+var njBounds = {west: -75.564, east: -73.885, south: 38.789, north: 41.357};
+var njCenter = {lat: 40.073, lng: -74.724};
+
+var maBounds = {west: -73.508, east: -69.928, south: 41.239, north: 42.887};
+var maCenter = {lat: 42.063, lng: -71.718};
+
+var msBounds = {west: -91.655, east: -88.098, south: 30.174, north: 34.996};
+var msCenter = {lat: 32.585, lng: -89.876};
+
+var vaBounds = {west: -83.675, east: -75.166, south: 36.541, north: 39.466};
+var vaCenter = {lat: 38.003, lng: -79.421};
  
