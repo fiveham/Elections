28617909b231cefb8b2daf1af5aa6dc0ba46ef3a
fiveham <fiveham@users.noreply.github.com>
2019-12-23 01:39:02 -0500

    say var a lot

--- docs/resources/bounds/bounds.js
+++ docs/resources/bounds/bounds.js
@@ -6,10 +6,10 @@
  * a human copies a single line into a script.
  */
  
- laBounds = {west: -94.043, east: -88.758, south: 28.855, north: 33.020};
- ncBounds = {west: -82.745, east: -75.887, south: 34.805, north: 36.562};
- njBounds = {west: -75.564, east: -73.885, south: 38.789, north: 41.357};
- maBounds = {west: -73.508, east: -69.928, south: 41.239, north: 42.887};
- msBounds = {west: -91.655, east: -88.098, south: 30.174, north: 34.996};
- vaBounds = {west: -83.675, east: -75.166, south: 36.541, north: 39.466};
+ var laBounds = {west: -94.043, east: -88.758, south: 28.855, north: 33.020};
+ var ncBounds = {west: -82.745, east: -75.887, south: 34.805, north: 36.562};
+ var njBounds = {west: -75.564, east: -73.885, south: 38.789, north: 41.357};
+ var maBounds = {west: -73.508, east: -69.928, south: 41.239, north: 42.887};
+ var msBounds = {west: -91.655, east: -88.098, south: 30.174, north: 34.996};
+ var vaBounds = {west: -83.675, east: -75.166, south: 36.541, north: 39.466};
  
