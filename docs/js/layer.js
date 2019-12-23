407c806494cb9036a9817191c0c0a3b2464e9566
fiveham <fiveham@users.noreply.github.com>
2019-12-23 01:21:20 -0500

    optional parameter: urls + kmllayer function

--- docs/js/layer.js
+++ docs/js/layer.js
@@ -1,8 +1,15 @@
 /* Wraps multiple KmlLayers in a single object to easily 
    turn a multi-file layer on or off. */
 class Layer{
-  constructor(members){
-    this.members = members;
+  constructor(members, func){
+    if(func){
+      this.members = [];
+      for(var i=0; i<members.length; i++){
+        this.members.push(func(members[i]));
+      }
+    } else{
+      this.members = members;
+    }
   }
   setMap(map){
     this.members.forEach(function(e){
