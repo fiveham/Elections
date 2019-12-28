6f66fcc2cd83cfe3da4ca860eb1b1915a8690558
fiveham <fiveham@users.noreply.github.com>
2019-12-28 10:55:23 -0500

    actually get smart about what state to set

--- docs/js/popups.js
+++ docs/js/popups.js
@@ -2,8 +2,10 @@
 
 /* Switch this popup on and the others off or switch this one off if it is already on. */
 function toggle_popup(element_id){
+  var element = document.getElementById(element_id);
+  var state = element.style.visibility === 'hidden' ? 'visible' : 'hidden';
   for(var i=0; i<popups.length; i++){
     document.getElementById(popups[i].id).style.visibility = 'hidden';
   }
-  document.getElementById(element_id).style.visibility = 'visible';
+  element.style.visibility = state;
 }
