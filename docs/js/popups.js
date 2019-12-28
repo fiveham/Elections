e346569d44f96c527ca6809e38ddef8381df2e79
fiveham <fiveham@users.noreply.github.com>
2019-12-28 10:42:00 -0500

    the popups list isn't DOM elements

--- docs/js/popups.js
+++ docs/js/popups.js
@@ -3,7 +3,7 @@
 /* Switch this popup on and the others off or switch this one off if it is already on. */
 function toggle_popup(element_id){
   for(var i=0; i<popups.length; i++){
-    popups[i].style.visibility = 'hidden';
+    document.getElementById(popups[i].id).style.visibility = 'hidden';
   }
   document.getElementById(element_id).style.visibility = 'visible';
 }
