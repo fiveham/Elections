e95b3c30414c669a97f568af7fb68a2d03665060
fiveham <fiveham@users.noreply.github.com>
2019-12-28 10:14:59 -0500

    Create popups.js

---
+++ docs/js/popups.js
@@ -0,0 +1,9 @@
+/* Depends on: global var 'popups': a list of objects each of which has an 'id' */
+
+/* Switch this popup on and the others off or switch this one off if it is already on. */
+function toggle_popup(element_id){
+  for(var i=0; i<popups.length; i++){
+    popups[i].style.visibility = 'hidden';
+  }
+  document.getElementById(element_id).style.visibility = 'visible';
+}
